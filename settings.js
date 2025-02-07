683b5f020efa529dceb4550f8599b5b757f640fe
freelancerkashifkhan <rediswolf7@gmail.com>
2025-02-07 21:53:04 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Knight Bot', // Your name
-  ownerNumber: '923497818013', //Your number
+  ownerNumber: '447563867976', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
